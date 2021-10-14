a313078ced2b6d2d4cc227261c9810d4126ce76a
risdallseis <rory.mot@hotmail.co.uk>
2021-10-15 00:05:04 +0100

    big old one

--- modules/dbscan_grid_search.py
+++ modules/dbscan_grid_search.py
@@ -1,3 +1,4 @@
+from numpy.lib.arraysetops import isin
 from sklearn.cluster import DBSCAN
 import random
 from modules.evaluate_model import get_eval_scores
@@ -17,7 +18,7 @@ def run_dbscan_gs(
     epsilon_range - range of floats
     min_sample_range - range of floats
     """
-    results = pd.DataFrame(columns=['iteration','category','precision','recall','eps','m_samples_divisor','features','no_clusters'])
+    results = pd.DataFrame(columns=['iteration','category','precision','recall','f1','eps','m_samples_divisor','features','no_clusters'])
     # FIRST LOOP TO ASSIGN VALUE IN RANGE TO EPS AND MIN_SAMPLES
     for iteration in range(0, iterations):
         epsilon = random.choice(epsilon_range)
@@ -29,11 +30,12 @@ def run_dbscan_gs(
             df['db_clust'] = pd.Series(model.fit_predict(df[features]), index=df.index)
             no_clusters = len(df['db_clust'].value_counts())
             try:
-                pscore, rscore = get_eval_scores(df['y_true'], df['db_clust'])
+                pscore, rscore, f1score = get_eval_scores(df['y_true'], df['db_clust'])
                 interim_results['iteration'] = iteration
                 interim_results['category'] = df.name
                 interim_results['precision'] = pscore
                 interim_results['recall'] = rscore
+                interim_results['f1'] = f1score
                 interim_results['eps'] = epsilon
                 interim_results['m_samples_divisor'] = m_samples
                 interim_results['features'] = [features]
@@ -55,15 +57,81 @@ def get_best_models(results):
         if len(results.loc[results['iteration']==i])==4:
             interim_results['precision_mean'] = results.loc[results['iteration']==i]['precision'].mean()
             interim_results['recall_mean'] = results.loc[results['iteration']==i]['recall'].mean()
+            interim_results['f1_mean'] = results.loc[results['iteration']==i]['f1'].mean()
             interim_results['epsilon'] = results.loc[results['iteration']==i]['eps'].values[0]
             interim_results['m_samples_divisor'] = results.loc[results['iteration']==i]['m_samples_divisor'].values[0]
             result_df = pd.DataFrame(data=interim_results, index=[0])
             best_models = pd.concat([best_models, result_df],axis=0)
         else:
             pass
-    best_models['f1_score'] = (2*best_models['precision_mean']*best_models['recall_mean'])/(best_models['precision_mean']+best_models['recall_mean'])
     #best_models['combined_score'] = (best_models['precision_mean']+best_models['recall_mean'])/2
     best_models = best_models.drop_duplicates()
-    best_models = best_models.sort_values(by=['f1_score'], axis=0, ascending=False)
+    best_models = best_models.sort_values(by=['f1_mean'], axis=0, ascending=False)
 
     return best_models
+
+
+def run_dbscan_gs_noeval(
+    dataframes,
+    features,
+    epsilon_range,
+    min_sample_range,
+    iterations,
+    outlier_prop_target,
+):
+    """This function performs a random grid search for epsilson and min_samples
+        this optimisation problem has the bound that only two clusters may form.
+    dataframes - LIST 
+    features - LIST
+    epsilon_range - range of floats
+    min_sample_range - range of floats
+    """
+    results = pd.DataFrame(columns=['iteration','category','eps','m_samples_divisor',
+    'features','no_clusters','outlier_prop','precision','recall','f1'])
+    # FIRST LOOP TO ASSIGN VALUE IN RANGE TO EPS AND MIN_SAMPLES
+    for iteration in range(0, iterations):
+        epsilon = random.choice(epsilon_range)
+        m_samples = random.choice(min_sample_range)
+        # SECOND LOOP TO TRY PARAMS FOR EACH DATAFRAME
+        for df in dataframes:
+            interim_results = {}
+            model = DBSCAN(eps=epsilon, min_samples=(len(df))/m_samples)
+            df['db_clust'] = pd.Series(model.fit_predict(df[features]), index=df.index)
+            no_clusters = len(df['db_clust'].value_counts())
+            outlier_prop = int((len(df.loc[df['db_clust']==-1])/len(df))*100)
+            try:
+                interim_results['iteration'] = iteration
+                interim_results['category'] = df.name
+                interim_results['eps'] = epsilon
+                interim_results['m_samples_divisor'] = m_samples
+                interim_results['features'] = [features]
+                interim_results['no_clusters'] = no_clusters
+                interim_results['outlier_prop'] = outlier_prop
+                pscore, rscore, f1score = get_eval_scores(df['y_true'], df['db_clust'])
+                interim_results['precision'] = pscore
+                interim_results['recall'] = rscore
+                interim_results['f1'] = f1score
+
+                if outlier_prop in range(outlier_prop_target-5,outlier_prop_target+5,1):
+                    if no_clusters ==2:
+                        result_df = pd.DataFrame(data=interim_results)
+                        results = pd.concat([results, result_df], axis=0)
+            except ValueError as err:
+                print(err.args)
+                pass
+    results.drop_duplicates(['category','eps','m_samples_divisor','no_clusters','outlier_prop'],inplace=True)
+
+    return results
+
+
+def get_best_noevals(results):
+    """finds best models for models without evaluation - using mininmum eps param"""
+    df = pd.DataFrame(columns=['iteration','category','eps','m_samples_divisor',
+    'features','no_clusters','outlier_prop','precision','recall','f1'
+    ])
+    cats = ['laptops', 'phones', 'tablets','desktops']
+    for c in cats:
+        df = pd.concat([df, results[results['category']==c].nsmallest(1,'eps')])
+    
+    return df
+
--- modules/feature_eng.py
+++ modules/feature_eng.py
@@ -32,8 +32,8 @@ def no_positive_reviews(list_scores):
 def get_y_true(df):
 	"""Get true y values based on total_sales zscore"""
 	df['zscore'] = (df['TOTAL_SALES']-df['TOTAL_SALES'].mean())/df['TOTAL_SALES'].std()
-	df.loc[(df['zscore'] >=1), 'y_true'] = -1
-	df.loc[(df['zscore'] <1), 'y_true'] = 0
+	df.loc[(df['zscore'] >=1), 'y_true'] = int(-1)
+	df.loc[(df['zscore'] <1), 'y_true'] = int(0)
 
 	return df.drop('zscore', axis=1)
 
@@ -42,13 +42,28 @@ def generate_features(df):
     """Generates some features after preproc"""
     df['neg_reviews'] = df['review_rating'].apply(lambda x: no_negative_reviews(x))
     df['pos_reviews'] = df['review_rating'].apply(lambda x: no_positive_reviews(x))
+
     df['price/Rvol'] = df['price']/df['no_reviews']
-    df['Rvol/%rec'] = df['no_reviews']/df['recommendation_percent']
+    df['%rec/Rvol'] = df['recommendation_percent']/df['no_reviews']
     df['posR/Rvol'] = df['pos_reviews']/df['no_reviews']
     df['negR/Rvol'] = df['neg_reviews']/df['no_reviews']
     
     return df
 
+
+def generate_features2(df):
+    """Generates some features after preproc"""
+    #df['neg_reviews'] = df['review_rating'].apply(lambda x: no_negative_reviews(x))
+    #df['pos_reviews'] = df['review_rating'].apply(lambda x: no_positive_reviews(x))
+    #df.loc[(df['neg_reviews'] ==0), 'neg_reviews'] = 1
+    #df.loc[(df['pos_reviews'] ==0), 'pos_reviews'] = 1
+    #df.loc[(df['no_reviews'] ==0), 'no_reviews'] = 1
+    df['Rvol/price'] = df['no_reviews']/df['price']
+    df['Rvol/%rec'] = df['no_reviews']/df['recommendation_percent']
+    #df['Rvol/posR'] = df['no_reviews']/df['pos_reviews']
+    #df['Rvol/negR'] = df['no_reviews']/df['neg_reviews']
+    
+    return df
 
 def do_PCA(
 	df,
@@ -61,6 +76,51 @@ def do_PCA(
 	pca = PCA(no_components)
 	df1 = df[['pos_reviews','neg_reviews', 'price/Rvol', 'Rvol/%rec', 'posR/Rvol', 
 			'negR/Rvol','price','no_reviews','recommendation_percent',
+			'summary_star_rating','TOTAL_SALES','product_name'
+			]]
+	if keep_no_reviews == False:
+		df1 = df1.loc[df1.no_reviews !=0]
+	sales = df1['TOTAL_SALES']
+	names = df1['product_name']
+	df1 = df1.drop(['TOTAL_SALES','product_name'], axis=1)
+	df1 = (df1-df1.mean())/df1.std()
+	df1.fillna(0, inplace=True)
+	pC = pca.fit_transform(df1)
+	df_pc = pd.DataFrame(data = pC, columns = ['c1','c2'])
+	if return_original_df == False:
+		df_pc.reset_index(inplace=True)
+		sales.reset_index(inplace=True)
+		names.reset_index(inplace=True)
+		pc_df = pd.concat([df_pc, sales,names], axis=1, ignore_index=False)
+	else:
+		df_pc.reset_index( inplace=True)
+		df.reset_index(inplace=True)
+		pc_df = pd.concat([df, df_pc],axis=1, ignore_index=False)
+
+
+	return pc_df
+
+
+def do_PCA2(
+	df,
+	keep_no_reviews=True,
+	return_original_df=True,
+	no_components = 2,
+):
+	"""Performs principal component analysis on data. Fills NA with 0.
+	"""
+	pca = PCA(no_components)
+	df['neg_reviews'] = df['review_rating'].apply(lambda x: no_negative_reviews(x))
+	df['pos_reviews'] = df['review_rating'].apply(lambda x: no_positive_reviews(x))
+	df.loc[(df['neg_reviews'] ==0), 'neg_reviews'] = 1
+	df.loc[(df['pos_reviews'] ==0), 'pos_reviews'] = 1
+	df.loc[(df['no_reviews'] ==0), 'no_reviews'] = 1
+	df['Rvol/price'] = df['no_reviews']/df['price']
+	df['Rvol/%rec'] = df['no_reviews']/df['recommendation_percent']
+	df['Rvol/posR'] = df['no_reviews']/df['pos_reviews']
+	df['Rvol/negR'] = df['no_reviews']/df['neg_reviews']
+	df1 = df[['pos_reviews','neg_reviews', 'Rvol/price', 'Rvol/%rec', 'Rvol/posR', 
+			'Rvol/negR','price','no_reviews','recommendation_percent',
 			'summary_star_rating','TOTAL_SALES','product_name'
 			]]
 	if keep_no_reviews == False:
